ef431c020c20ff31c3434c446a185407da380891
Vishal <vishalkumar916171@gmail.com>
2026-08-11 15:52:25 +0530

    Employee Management System Admin panel update

--- EMS/ems/src/components/other/AllTask.jsx
+++ EMS/ems/src/components/other/AllTask.jsx
@@ -1,55 +1,39 @@
 // import React from 'react'
 
+import { useContext} from "react"
+import { AuthContext } from "../../context/AuthProvider"
+
+
 const AllTask = () => {
+   const AuthData = useContext(AuthContext)
+
+//    console.log(AuthData)
   return (
-    <div className="bg-gray-300 mt-5 rounded-xl p-5 overflow-auto h-40">
 
-        <div className="bg-red-400 flex justify-between p-2 rounded"> 
-            <h1>Vishal</h1>
-            <h2>Make a UI design</h2>
-            <h2>Status</h2>
-        </div>
+    <div className="bg-gray-300 mt-5  rounded-xl p-5 "> 
 
-        
-        <div className="bg-green-400 flex justify-between mt-2 p-2 rounded"> 
-            <h1>Vishal</h1>
-            <h2>Make a UI design</h2>
-            <h2>Status</h2>
-        </div>
+                <div className="bg-red-400 mt-3 flex font-medium justify-between p-2 rounded">
+                    <h2 className="w-1/5 ">Employee Name</h2>
+                    <h2 className="w-1/5">New Task</h2>
+                    <h2 className="w-1/5">Active Task</h2>
+                    <h2 className="w-1/5">Completed</h2>
+                    <h2 className="w-1/5">Failed</h2>
+                </div>
 
-        
-        <div className="bg-pink-400 flex justify-between mt-2 p-2 rounded"> 
-            <h1>Vishal</h1>
-            <h2>Make a UI design</h2>
-            <h2>Status</h2>
-        </div>
 
-        
-        <div className="bg-yellow-400 flex justify-between mt-2 p-2 rounded"> 
-            <h1>Vishal</h1>
-            <h2>Make a UI design</h2>
-            <h2>Status</h2>
+           <div className="h-[80%] overflow-auto">
+             {AuthData.employees.map(function(elem,idx){
+            return  <div key={idx} className=" mt-3 border-2 border-blue-400 flex justify-between px-8 py-2  rounded"> 
+                    <h2 className="w-1/5 text-md font-semibold ">{elem.firstName}</h2>
+                    <h2 className="w-1/5 text-blue-700">{elem.taskNumber.newTask}</h2>
+                    <h2 className="w-1/5 text-orange-500">{elem.taskNumber.active}</h2>
+                    <h2 className="w-1/5 text-green-600">{elem.taskNumber.completed}</h2>
+                    <h2 className="w-1/5 text-red-400">{elem.taskNumber.failed}</h2>
         </div>
-
-        <div className="bg-purple-400 flex justify-between mt-2 p-2 rounded"> 
-            <h1>Vishal</h1>
-            <h2>Make a UI design</h2>
-            <h2>Status</h2>
-        </div>
+            })}
+           </div>
 
         
-        <div className="bg-blue-400 flex justify-between mt-2 p-2 rounded"> 
-            <h1>Vishal</h1>
-            <h2>Make a UI design</h2>
-            <h2>Status</h2>
-        </div>
-
-        
-        <div className="bg-orange-400 flex justify-between mt-2 p-2 rounded"> 
-            <h1>Vishal</h1>
-            <h2>Make a UI design</h2>
-            <h2>Status</h2>
-        </div>
 
     </div>
   )
