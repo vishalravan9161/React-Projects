7d6dc70561be470c7791a25d6392f073e65aedb6
vishal Kumar <vishalkumar916171@gmail.com>
2026-08-29 12:13:27 +0530

    gsap animation apply on image tag

--- K72Web/src/Pages/Agence.jsx
+++ K72Web/src/Pages/Agence.jsx
@@ -8,13 +8,36 @@ import { useRef } from "react";
 const Agence = () => {
   gsap.registerPlugin(ScrollTrigger)
   const imageDivRef = useRef(null)
+  const imageRef  = useRef(null)
+  const imageArray = [
+        'https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7',
+        'https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64',
+        'https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991',
+        'https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2',
+        'https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6',
+        'https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a',
+        'https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847',
+        'https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6',
+  ]
+  // console.log(imageArray)
 
   useGSAP(function(){
     gsap.to(imageDivRef.current,{
      scrollTrigger:{
       trigger:imageDivRef.current,
-      markers:true
-
+      start: 'top 22%',
+      end: 'top -140%',
+      scrub: true,
+      pin: true,
+      onUpdate:function(elem){
+        let imageIndex;
+       if(elem.progress<1){
+         imageIndex = Math.floor(elem.progress * imageArray.length);
+       }else {
+        imageIndex = imageArray.length - 1;
+       }
+        imageRef.current.src = imageArray[imageIndex]
+      }
      }
     })
   })
@@ -22,7 +45,7 @@ const Agence = () => {
    <div>
     <div className="sectoion1">
     <div ref={imageDivRef} className="h-[20vw] w-[15vw] overflow-hidden rounded-2xl absolute top-35 left-102">
-      <img className="rounded-2xl h-full w-full" src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+      <img ref={imageRef} className="rounded-2xl h-full w-full" src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
     </div>
      <div className=" relative font-[font2]">
       <div className="mt-[55vh] ">
@@ -31,7 +54,7 @@ const Agence = () => {
       </div>
 
       <div className="mt-5">
-        <p className="text-[4vw] leading-[4vw] ml-[42%]">
+        <p className="text-[3.5vw] leading-[4vw] ml-[42%]">
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We're inquisitive and open-minded, and we make sure creativity
            crowds out ego from every corner. A brand is a living thing, with 
