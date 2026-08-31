4b5939bd67c4a4987719a99dca9dc3bc448914d1
vishal Kumar <vishalkumar916171@gmail.com>
2026-08-31 22:01:14 +0530

    close svg implemetation

--- K72Web/src/components/Navigation/FullScreenNav.jsx
+++ K72Web/src/components/Navigation/FullScreenNav.jsx
@@ -2,8 +2,25 @@
 
 const FullScreenNav = () => {
   return (
-    <div className="bg-black text-white">
-        <div id="fullscreennav" className="h-screen  overflow-hidden py-30 w-full absolute bg-black">
+    <div className=" full bg-black text-white">
+       <div className="">
+        <div className=" flex justify-between  h-30">
+        <svg className="ml-2 " xmlns="http://www.w3.org/2000/svg" width="115" height="78" viewBox="0 0 103 44">
+            <path fill="white" fillRule="evenodd" d="M35.1441047,8.4486911 L58.6905011,8.4486911 L58.6905011,-1.3094819e-14 L35.1441047,-1.3094819e-14 L35.1441047,8.4486911 Z M20.0019577,0.000230366492 L8.83414254,25.3433089 L18.4876971,25.3433089 L29.5733875,0.000230366492 L20.0019577,0.000230366492 Z M72.5255345,0.000691099476 L72.5255345,8.44846073 L94.3991559,8.44846073 L94.3991559,16.8932356 L72.5275991,16.8932356 L72.5275991,19.5237906 L72.5255345,19.5237906 L72.5255345,43.9274346 L102.80937,43.9274346 L102.80937,35.4798953 L80.9357483,35.4798953 L80.9357483,25.3437696 L94.3996147,25.3428482 L94.3996147,16.8953089 L102.80937,16.8953089 L102.80937,0.000691099476 L72.5255345,0.000691099476 Z M-1.30398043e-14,43.9278953 L8.78642762,43.9278953 L8.78642762,0.0057591623 L-1.30398043e-14,0.0057591623 L-1.30398043e-14,43.9278953 Z M58.6849955,8.4486911 L43.1186904,43.9274346 L52.3166592,43.9274346 L67.9877996,8.4486911 L58.6849955,8.4486911 Z M18.4688864,25.3437696 L26.7045278,43.9278953 L36.2761871,43.9278953 L28.1676325,25.3375497 L18.4688864,25.3437696 Z"></path>
+          </svg>
+        
+         <button className="">
+           <svg  className="cursor-pointer hover:text-[#D3FD50]" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="110" height="110" viewBox="0 0 50 50">
+             <path fill="currentColor" d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path>
+              </svg>
+         </button>
+           {/* <div className="bg-white hover:bg-[#D3FD50]  h-0.5 w-35  rotate-45"></div>
+           <div className="bg-white hover:bg-[#D3FD50] h-0.5 w-35  -rotate-45"></div> */}
+         
+       </div>
+       </div>
+        
+        <div id="fullscreennav" className="h-screen overflow-hidden w-full absolute bg-black">
           <div className="link border-t leading-20 relative ">
             <h1 className="font-[font2] text-[8vw] mt-3  text-center uppercase">Projects</h1>
             <div className="moveLink absolute  flex  top-0 bg-[#D3FD50] text-black">
