2015591d2cebc232bb54807054f8fe928448251c
vishal Kumar <vishalkumar916171@gmail.com>
2026-08-31 17:28:54 +0530

    fullscreennav animation apply

--- K72Web/src/components/Navigation/FullScreenNav.jsx
+++ K72Web/src/components/Navigation/FullScreenNav.jsx
@@ -3,10 +3,10 @@
 const FullScreenNav = () => {
   return (
     <div className="bg-black text-white">
-        <div id="fullscreennav" className="h-screen  py-30 w-full absolute bg-amber-800">
-          <div className="links border-t leading-20 relative ">
-            <h1 className="font-[font2] text-[7vw] mt-3  text-center uppercase">Projects</h1>
-            <div className="absolute flex  top-0 bg-[#D3FD50] text-black">
+        <div id="fullscreennav" className="h-screen  overflow-hidden py-30 w-full absolute bg-black">
+          <div className="link border-t leading-20 relative ">
+            <h1 className="font-[font2] text-[8vw] mt-3  text-center uppercase">Projects</h1>
+            <div className="moveLink absolute  flex  top-0 bg-[#D3FD50] text-black">
                 <div className=" moveX flex items-center">
                   <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">SEE EVERYTHING</h2>
                   <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="" />
@@ -18,6 +18,63 @@ const FullScreenNav = () => {
                   <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="" />
                   <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">SEE EVERYTHING</h2>
                   <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=640&h=290&s=755b635c06d126151d64017fa1042a7c" alt="" />
+                </div>
+                
+            </div>
+          </div>
+
+          <div className="link border-t leading-20 relative ">
+            <h1 className="font-[font2] text-[8vw] mt-3  text-center uppercase">Agency</h1>
+            <div className="moveLink absolute  flex  top-0 bg-[#D3FD50] text-black">
+                <div className=" moveX flex items-center">
+                  <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">Know Us</h2>
+                  <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://k72.ca/images/teamMembers/blank_copie_2.jpg?w=640&h=290&s=b6f8d41383b2ee7821dcaec8b68295ec" alt="" />
+                  <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">Know Us</h2>
+                  <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://k72.ca/images/teamMembers/MEL_640X290.jpg?w=640&h=290&s=c18878a98fbce289bc80effb56cfbd81" alt="" />
+                </div>
+                 <div className=" moveX flex items-center">
+                  <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">Know Us</h2>
+                  <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://k72.ca/images/teamMembers/blank_copie_2.jpg?w=640&h=290&s=b6f8d41383b2ee7821dcaec8b68295ec" alt="" />
+                  <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">Know Us</h2>
+                  <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://k72.ca/images/teamMembers/MEL_640X290.jpg?w=640&h=290&s=c18878a98fbce289bc80effb56cfbd81" alt="" />
+                </div>
+                
+            </div>
+          </div>
+
+          <div className="link border-t leading-20 relative ">
+            <h1 className="font-[font2] text-[8vw] mt-3  text-center uppercase">Contact</h1>
+            <div className="moveLink absolute  flex  top-0 bg-[#D3FD50] text-black">
+                <div className=" moveX flex items-center">
+                  <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">Send Us A Fax</h2>
+                  <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://media.istockphoto.com/id/1450058572/photo/businessman-using-a-laptop-and-touching-on-virtual-screen-contact-icons-consists-of-telephone.jpg?s=612x612&w=0&k=20&c=R5wzCGHu6ZS-8EQpJ2Z1tkSbKGGdJH4apVhFM18EXSM=" alt="" />
+                  <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">Send Us A Fax</h2>
+                  <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://media.istockphoto.com/id/1015358064/photo/website-contact-us-concept-with-wooden-blocks.jpg?s=612x612&w=0&k=20&c=zgN6OOItg8FXd6nmNsklQr_Dl4DZ13ndP5RpAPsv8lQ=" alt="" />
+                </div>
+                 <div className=" moveX flex items-center">
+                  <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">Send Us A Fax</h2>
+                  <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://media.istockphoto.com/id/1015358064/photo/website-contact-us-concept-with-wooden-blocks.jpg?s=612x612&w=0&k=20&c=zgN6OOItg8FXd6nmNsklQr_Dl4DZ13ndP5RpAPsv8lQ=" alt="" />
+                  <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">Send Us A Fax</h2>
+                  <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://media.istockphoto.com/id/1450058572/photo/businessman-using-a-laptop-and-touching-on-virtual-screen-contact-icons-consists-of-telephone.jpg?s=612x612&w=0&k=20&c=R5wzCGHu6ZS-8EQpJ2Z1tkSbKGGdJH4apVhFM18EXSM=" alt="" />
+                </div>
+                
+            </div>
+          </div>
+
+          <div className="link border-y leading-20 relative ">
+            <h1 className="font-[font2] text-[8vw] mt-3  text-center uppercase">Blog</h1>
+            <div className="moveLink absolute  flex  top-0 bg-[#D3FD50] text-black">
+                <div className=" moveX flex items-center">
+                  <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">Read Articles</h2>
+                  <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://k72.ca/images/blog/blogImg/K72_article_ChatGPT_blogue.jpg?w=640&h=290&s=cec2aa341c22369e36e602c558c49e2a" alt="" />
+                  <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">Read Articles</h2>
+                  <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://k72.ca/images/blog/blogImg/50ff59cc0550df5b36543807a58db98c52e01a22274a317eafbfa5266941579b.png?w=640&h=290&s=4f8134f04fe18db7382b99cec63c95f5" alt="" />
+                </div>
+                 <div className=" moveX flex items-center">
+                  <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">Read Articles</h2>
+                  <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://k72.ca/images/blog/blogImg/K72_article_ChatGPT_blogue.jpg?w=640&h=290&s=cec2aa341c22369e36e602c558c49e2a" alt="" />
+                  <h2 className="font-[font2] whitespace-nowrap text-[7vw] mt-3 text-center uppercase">Read Articles</h2>
+                  <img className="h-20 rounded-full ml-3 mr-3 object-cover w-50 shrink-0" src="https://k72.ca/images/blog/blogImg/50ff59cc0550df5b36543807a58db98c52e01a22274a317eafbfa5266941579b.png?w=640&h=290&s=4f8134f04fe18db7382b99cec63c95f5" alt="" />
                 </div>
                 
             </div>
